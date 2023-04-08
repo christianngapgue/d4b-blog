5e5ac59ea9b38f7aed4586463a4b5e6ef88a9106
Christian NGAPGUE <christianngapgue@gmail.com>
2023-04-08 17:18:14 +0100

    Wire a URL pattern to the BlogDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import BlogListView
+from blog.views import BlogDetailView, BlogListView
 
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
