bd7707363f039c0c9ebbd58a1c587a102b6bcd86
Christian NGAPGUE <christianngapgue@gmail.com>
2023-04-09 00:57:54 +0100

    Wire up the template with the BlogCreateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import BlogDetailView, BlogListView
+from blog.views import BlogCreateView, BlogDetailView, BlogListView
 
 urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name="post_new"),
     path('', BlogListView.as_view(), name='home'),
 ]
