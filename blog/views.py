75393030f947d4124f0b417df442ab4fbc6d6905
Christian NGAPGUE <christianngapgue@gmail.com>
2023-04-09 01:14:08 +0100

    Add a view for editing a blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from blog.models import Post
 
@@ -15,3 +15,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
